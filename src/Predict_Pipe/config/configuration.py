64be888721522271c3f26f71876f6e630b9ec0b7
Yash Maini <mainiyash2@gmail.com>
2025-04-01 15:37:13 +0530

    setup config,components,pipelines,entity for model training

--- src/Predict_Pipe/config/configuration.py
+++ src/Predict_Pipe/config/configuration.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 from src.Predict_Pipe.constants import *
 from src.Predict_Pipe.utils.common import read_yaml, create_directories
-from src.Predict_Pipe.entity.config_entity import DataIngestionConfig, DataValidationConfig, DataTransformationConfig
+from src.Predict_Pipe.entity.config_entity import DataIngestionConfig, DataValidationConfig, DataTransformationConfig, ModelTrainerConfig
 
 class ConfigurationManager:
     def __init__(
@@ -65,3 +65,22 @@ class ConfigurationManager:
         )
 
         return data_transformation_config
+    
+    def get_model_trainer_config(self) -> ModelTrainerConfig:
+        config = self.config.model_trainer
+        params = self.params.ElasticNet
+        schema =  self.schema.TARGET_COLUMN
+
+        create_directories([config.root_dir])
+        
+        model_trainer_config = ModelTrainerConfig(
+            root_dir=Path(config.root_dir),
+            train_data_path=Path(config.train_data_path),
+            test_data_path=Path(config.test_data_path),
+            model_name=config.model_name,
+            alpha=params.alpha,
+            l1_ratio=params.l1_ratio,
+            target_column=schema
+        )
+
+        return model_trainer_config
